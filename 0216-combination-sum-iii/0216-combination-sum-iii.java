a886b9f0788eb8e068a7ff81803e9baaf36ad3bc
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-04-15 01:42:36 +0530

    Time: 0 ms (100%), Space: 41.1 MB (43.31%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -5,17 +5,16 @@ class Solution {
         return ans;
         
     }
-    public void recursion(int i, int k, int n, List<Integer> curr, List<List<Integer>> ans){
-        if(n==0 && k==0){
-            ans.add(curr);
+    public void recursion(int start, int k, int n, List<Integer> curr, List<List<Integer>> ans){
+        if(curr.size()==k && n==0){
+            List<Integer> temp=new ArrayList<Integer>(curr);
+            ans.add(temp);
             return;
         }
-        if(i>9)return;
-        if(n<0 || k<0)return;
-
-        List<Integer> temp=new ArrayList<>(curr);
-        temp.add(i);
-        recursion(i+1,k-1,n-i,temp,ans);
-        recursion(i+1,k,n,curr,ans);
+        for(int i=start;i<=9;i++){
+            curr.add(i);
+        recursion(i+1,k,n-i,curr,ans);
+        curr.remove(curr.size()-1);
+    }
     }
 }
