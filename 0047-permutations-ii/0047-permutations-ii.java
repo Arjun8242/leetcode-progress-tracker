341a64de50aed2dc65429fb0b0e589f1f013fed0
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-04-16 02:09:58 +0530

    Time: 2 ms (72.64%), Space: 44.8 MB (72.13%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        Arrays.sort(nums);
+        Boolean[] Map=new Boolean[nums.length];
+        Arrays.fill(Map, false);
+        recursive(nums,ans,temp,Map);
+        return ans; 
+    }
+     void recursive(int[] nums, List<List<Integer>> ans, List<Integer> temp,  Boolean[] Map){
+        if(temp.size()==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i > 0 && !Map[i-1] && nums[i] == nums[i-1]) continue; 
+            if(Map[i]==false){
+                Map[i]=true;
+                temp.add(nums[i]);
+                recursive(nums, ans, temp, Map);
+                                Map[i]=false;
+                temp.remove(temp.size()-1);
+
+            }
+        }
+       
+    }
+}
