bd2963e59406d4c072c09f74b6afa8e0499eada9
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-05-12 01:20:00 +0530

    Time: 43 ms (46.52%), Space: 45 MB (24.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] memo=new int[nums.length][nums.length+1];
+       /* int[][] memo=new int[nums.length][nums.length+1];
         for(int[]arr:memo) Arrays.fill(arr, -1);
          return LIS(0,-1,nums,memo);
     }
@@ -13,7 +13,25 @@ class Solution {
         if(prev_ind==-1 || nums[curr_ind]>nums[prev_ind]){
             len=Math.max(len,1+LIS(curr_ind+1,curr_ind,nums,memo));
         }
-        return memo[curr_ind][prev_ind+1]=len;
+        return memo[curr_ind][prev_ind+1]=len;*/
+
+        if(nums.length==0){
+            return 0;
+        }
+        int n=nums.length;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,1);
+         
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+
+        int maxlength=Arrays.stream(dp).max().getAsInt();
+        return maxlength;
 
     }
 }
