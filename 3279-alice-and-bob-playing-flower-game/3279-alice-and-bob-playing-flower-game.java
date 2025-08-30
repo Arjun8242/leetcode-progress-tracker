aa0634d1af1b98b1324cbb5a9c0fe85e06752dd1
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-08-30 12:35:41 +0530

    Time: 0 ms (100%), Space: 40.5 MB (87.12%) - LeetHub

---
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -0,0 +1,7 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        return 1L*n*m/2;
+
+        /* you only have too pick those pairs which are distinct and has 1 odd and 1 even number in it cause 1 odd and 1even combo will make  alice win the game */ 
+    }
+}
