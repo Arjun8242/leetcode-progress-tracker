e980e8af55e2978ddc03336cb9a7149bc9e1a982
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-09-05 23:59:30 +0530

    Time: 0 ms (100%), Space: 44.9 MB (98.81%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+         int n = nums.length;
+        int[] freq = new int[n + 1]; 
+
+        for (int num : nums) {
+            freq[num] = 1;
+        }
+
+         for (int i = 0; i <= n; i++) {
+            if (freq[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
