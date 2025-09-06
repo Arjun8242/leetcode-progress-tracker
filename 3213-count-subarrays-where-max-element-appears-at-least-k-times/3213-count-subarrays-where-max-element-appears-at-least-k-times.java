cf49b5138c4766bb0263c1a5f7803b96796def4a
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-09-06 12:39:09 +0530

    Time: 5 ms (98.89%), Space: 64.3 MB (68.06%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,25 +1,22 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        int n = nums.length;
+         int n = nums.length;
         int maxVal = 0;
         for (int num : nums) {
             maxVal = Math.max(maxVal, num);
         }
-        for (int i = 0; i < n; i++) {
-            nums[i] = (nums[i] == maxVal) ? 1 : 0;
-        }
         long total = (long) n * (n + 1) / 2;
-        return total - atmost(nums, k - 1);/* the number of subarray where max appeared atleast k times is = total no. of subaaray - those subarray where max appeared k - 1 times*/
+        return total - atmost(nums, k - 1, maxVal); /* the number of subarray where max appeared atleast k times is = total no. of subaaray - those subarray where max appeared k - 1 times*/
     }
 
-    public long atmost(int[] nums, int k) {
+    public long atmost(int[] nums, int k, int maxVal) {
         int n = nums.length;
         long count = 0;
         int ones = 0, left = 0;
         for (int right = 0; right < n; right++) {
-            if (nums[right] == 1) ones++;
+            if (nums[right] == maxVal) ones++;
             while (ones > k) {
-                if (nums[left] == 1) ones--;
+                if (nums[left] == maxVal) ones--;
                 left++;
             }
             count += (right - left + 1);
