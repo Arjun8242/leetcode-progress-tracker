e9cd2bd9e4ddfd4588acb2bc52106c355a291d16
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-10-30 22:46:04 +0530

    Time: 71 ms (28.94%), Space: 65.9 MB (27.8%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,26 +2,46 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
         int[][][] dp=new int [n+1][3][2];//[n][trans][buy]
-        for(int[][] arr:dp){
-            for(int[] a:arr){
-                Arrays.fill(a, -1);
+    //     for(int[][] arr:dp){
+    //         for(int[] a:arr){
+    //             Arrays.fill(a, -1);
+    //         }
+    //     }
+    //     return recursion(0, 2, 1, n, prices, dp);
+    // }
+    // public int recursion(int day, int trans, int buy, int n, int[] prices,int[][][] dp){
+    //     if(day==n || trans==0) return 0;
+
+    //     if(dp[day][trans][buy]!=-1) return dp[day][trans][buy];
+
+    //     //buying phase 
+        // if(buy==1){
+        //     return dp[day][trans][buy] = Math.max(-prices[day]+recursion(day+1, trans, 0, n, prices, dp), recursion(day+1, trans, 1, n, prices, dp));
+        // }
+
+        // //selling phase
+        // else{
+        //     return dp[day][trans][buy] = Math.max(prices[day]+recursion(day+1, trans-1, 1, n, prices, dp), recursion(day+1, trans, 0, n, prices, dp));
+        // }
+
+
+    // tabulated approach 
+
+    for(int day=n-1;day>=0;day--){
+        for(int trans=1;trans<=2;trans++){
+            for(int buy=0;buy<=1;buy++){
+                //buying phase 
+                if(buy==1){
+                dp[day][trans][buy] = Math.max(-prices[day]+dp[day+1][trans][0], dp[day+1][trans][1]);
+                }
+
+                //selling phase
+                else{
+                    dp[day][trans][buy] = Math.max(prices[day]+dp[day+1][trans-1][1], dp[day+1][trans][0]);
+                    }
             }
         }
-        return recursion(0, 2, 1, n, prices, dp);
     }
-    public int recursion(int day, int trans, int buy, int n, int[] prices,int[][][] dp){
-        if(day==n || trans==0) return 0;
-
-        if(dp[day][trans][buy]!=-1) return dp[day][trans][buy];
-
-        //buying phase 
-        if(buy==1){
-            return dp[day][trans][buy] = Math.max(-prices[day]+recursion(day+1, trans, 0, n, prices, dp), recursion(day+1, trans, 1, n, prices, dp));
-        }
-
-        //selling phase
-        else{
-            return dp[day][trans][buy] = Math.max(prices[day]+recursion(day+1, trans-1, 1, n, prices, dp), recursion(day+1, trans, 0, n, prices, dp));
-        }
+    return dp[0][2][1];
     }
 }
