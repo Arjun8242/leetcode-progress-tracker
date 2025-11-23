be9a6db8c376758f775dcea8877064b983b05219
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2025-11-23 23:27:01 +0530

    Time: 3 ms (56.89%), Space: 101.4 MB (25.2%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,23 +1,43 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        int n=nums.length;
-        long[] sumleft=new long[n];
-        long[] sumright=new long[n];
+        // int n=nums.length;
+        // long[] sumleft=new long[n];
+        // long[] sumright=new long[n];
 
-        sumleft[0]=nums[0];
-        for(int i=1;i<n;i++){
-            sumleft[i]=sumleft[i-1]+nums[i];
+        // sumleft[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     sumleft[i]=sumleft[i-1]+nums[i];
+        // }
+
+        // sumright[n-1]=nums[n-1];
+        // for(int i=n-2;i>=0;i--){
+        //     sumright[i]=sumright[i+1]+nums[i];
+        // }
+
+        // int count=0;
+        // for(int i=1;i<n;i++){
+        //     if(sumleft[i-1]>=sumright[i]) count++;
+        // }
+        // return count;
+
+        int n = nums.length;
+
+        long[] prefix = new long[n];
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
         }
 
-        sumright[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            sumright[i]=sumright[i+1]+nums[i];
+        long total = prefix[n-1];
+        int count = 0;
+
+        for (int i = 0; i < n-1; i++) { 
+            long left = prefix[i];
+            long right = total - prefix[i];
+            if (left >= right) count++;
         }
 
-        int count=0;
-        for(int i=1;i<n;i++){
-            if(sumleft[i-1]>=sumright[i]) count++;
-        }
         return count;
     }
 }
