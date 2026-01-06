4d25752934814d7c5714c8bcba752b5f18a8ea76
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-01-06 19:32:27 +0530

    Time: 9 ms (64.42%), Space: 49.2 MB (38.96%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -15,31 +15,61 @@
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        int h=height(root);
-        int[] levelSum=new int[h+1];
-        dfs(root, 1, levelSum);
+    //     int h=height(root);
+    //     int[] levelSum=new int[h+1];
+    //     dfs(root, 1, levelSum);
 
-        int max=Integer.MIN_VALUE;
-        int ans=0;
+    //     int max=Integer.MIN_VALUE;
+    //     int ans=0;
 
-        for(int i=1;i<levelSum.length;i++){
-            if(levelSum[i]>max){
-                max=levelSum[i];
-                ans=i;
+    //     for(int i=1;i<levelSum.length;i++){
+    //         if(levelSum[i]>max){
+    //             max=levelSum[i];
+    //             ans=i;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    // public int height(TreeNode root){
+    //     if(root==null) return 0;
+    //     return 1+Math.max(height(root.left), height(root.right));
+    // }
+    // public void dfs(TreeNode root, int level, int[] levelSum){
+    //     if(root==null) return ;
+
+    //     levelSum[level]+=root.val;
+    //     dfs(root.left, level+1, levelSum);
+    //     dfs(root.right, level+1, levelSum);
+
+
+    Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int level = 1;
+        int answerLevel = 1;
+        int maxSum =Integer.MIN_VALUE;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
-        }
-        return ans;
-    }
-    public int height(TreeNode root){
-        if(root==null) return 0;
-        return 1+Math.max(height(root.left), height(root.right));
-    }
-    public void dfs(TreeNode root, int level, int[] levelSum){
-        if(root==null) return ;
 
-        levelSum[level]+=root.val;
-        dfs(root.left, level+1, levelSum);
-        dfs(root.right, level+1, levelSum);
+            if (sum > maxSum) {
+                maxSum = sum;
+                answerLevel = level;
+            }
+
+            level++;
+        }
+
+        return answerLevel;
     }
 
 }
