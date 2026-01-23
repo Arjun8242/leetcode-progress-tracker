2d59c6841619dd712efe7f7aa5a407e665fa3c9e
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-01-23 10:21:14 +0530

    Time: 1 ms (99.53%), Space: 47.6 MB (13.7%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,27 +1,42 @@
 class Solution {
     public int jump(int[] nums) {
-        int n=nums.length;
-        boolean[] vis=new boolean[n];
-        Queue<int[]> q=new LinkedList<>();
-        q.offer(new int[]{0,0});
-        vis[0]=true;
+        // int n=nums.length;
+        // boolean[] vis=new boolean[n];
+        // Queue<int[]> q=new LinkedList<>();
+        // q.offer(new int[]{0,0});
+        // vis[0]=true;
 
-        while(!q.isEmpty()){
-            int[] curr=q.poll();
-            int idx=curr[0];
-            int jumps=curr[1];
+        // while(!q.isEmpty()){
+        //     int[] curr=q.poll();
+        //     int idx=curr[0];
+        //     int jumps=curr[1];
 
-            if(idx==n-1) return jumps;
+        //     if(idx==n-1) return jumps;
             
-            int maxjumps=nums[idx];
-            for(int i=1;i<=maxjumps;i++){
-                int nxtidx=idx+i;
-                if(nxtidx<n && !vis[nxtidx]){
-                    vis[nxtidx]=true;
-                    q.offer(new int[]{nxtidx, jumps+1});
-                }
+        //     int maxjumps=nums[idx];
+        //     for(int i=1;i<=maxjumps;i++){
+        //         int nxtidx=idx+i;
+        //         if(nxtidx<n && !vis[nxtidx]){
+        //             vis[nxtidx]=true;
+        //             q.offer(new int[]{nxtidx, jumps+1});
+        //         }
+        //     }
+        // }
+        // return -1;
+
+        int n=nums.length;
+        int jumps=0;
+        int farthestEnd=0;
+        int currEnd=0;
+
+        for(int i=0;i<n-1;i++){
+            farthestEnd=Math.max(farthestEnd, i+nums[i]);
+
+            if(i==currEnd){
+                jumps++;
+                currEnd=farthestEnd;
             }
         }
-        return -1;
+        return jumps;
     }
 }
