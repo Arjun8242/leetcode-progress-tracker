4e0fb7581875fd603bb0d754e7ae081ddeebb80c
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-02-04 23:05:51 +0530

    Time: 3 ms (99.71%), Space: 47.6 MB (13.6%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,31 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-     int[] state=new int[numCourses];
-     List<List<Integer>> graph=new ArrayList<>();
-     for(int i=0;i<numCourses;i++){
-        graph.add(new ArrayList<>());
-    }   
-    for(int[] pre : prerequisites){
-        graph.get(pre[1]).add(pre[0]);
+        int[] state=new int[numCourses];
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] pre:prerequisites){
+            graph.get(pre[1]).add(pre[0]);
+        }
+
+        for(int i=0;i<numCourses;i++){
+            if(state[i]==0){
+                if(!dfs(graph, state, i)) return false;
+            }
+        }
+
+        return true;
     }
 
-     for(int i=0;i<numCourses;i++){
-        if(state[i]==0){
-            if(!dfs(graph, state, i))
-            return false;
-        }
-     }
-     return true;
-    }
     public boolean dfs(List<List<Integer>> graph, int[] state, int node){
         if(state[node]==1) return false;
         if(state[node]==2) return true;
         state[node]=1;
-        for(int neighbor:graph.get(node)){
-            if(!dfs(graph, state, neighbor))
-            return false;
+        
+        for(int next:graph.get(node)){
+            if(!dfs(graph, state, next))return false;
         }
+
         state[node]=2;
+
         return true;
     }
 }
