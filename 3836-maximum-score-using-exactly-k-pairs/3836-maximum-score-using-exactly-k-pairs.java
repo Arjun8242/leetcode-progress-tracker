3da1731044f00ce7518239b40e58cb2acab18cac
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-02-24 21:59:51 +0530

    Time: 151 ms (83.01%), Space: 172.4 MB (64.14%) - LeetHub

--- 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.java
+++ 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.java
@@ -1,24 +1,48 @@
 class Solution {
-    int m;
-    int n;
-    Long[][][] dp;
+    // int m;
+    // int n;
+    // Long[][][] dp;
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        m=nums1.length;
-        n=nums2.length;
-        dp=new Long[m+1][n+1][k+1];
-        return memo(nums1, nums2, 0, 0, k);
+        int m=nums1.length;
+        int n=nums2.length;
+        Long NEG=Long.MIN_VALUE/2;
+        long[][][] dp=new long[m+1][n+1][k+1];
+
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                for(int t=1;t<=k;t++){
+                    dp[i][j][t]=NEG;
+                }
+            }
+        }
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                for(int t=1;t<=k;t++){
+                    long pick=1L*nums1[i]*nums2[j]+dp[i+1][j+1][t-1];
+                    long skip1=dp[i+1][j][t];
+                    long skip2=dp[i][j+1][t];
+
+                    dp[i][j][t]=Math.max(pick, Math.max(skip1, skip2));
+                }
+            }
+        }
+
+        return dp[0][0][k];
+
+        // return memo(nums1, nums2, 0, 0, k);
     }
 
-    public long memo(int[] nums1, int[] nums2, int i, int j, int k){
-        if(k<=0) return 0;
-        if(i>=m || j>=n) return Long.MIN_VALUE/2;
+    // public long memo(int[] nums1, int[] nums2, int i, int j, int k){
+    //     if(k<=0) return 0;
+    //     if(i>=m || j>=n) return Long.MIN_VALUE/2;
 
-        if(dp[i][j][k]!=null) return dp[i][j][k];
+    //     if(dp[i][j][k]!=null) return dp[i][j][k];
 
-        long pick=nums1[i]*nums2[j]+memo(nums1, nums2, i+1, j+1, k-1);
-        long skip1=memo(nums1, nums2, i+1, j, k);
-        long skip2=memo(nums1, nums2, i, j+1, k);
+    //     long pick=1L*nums1[i]*nums2[j]+memo(nums1, nums2, i+1, j+1, k-1);
+    //     long skip1=memo(nums1, nums2, i+1, j, k);
+    //     long skip2=memo(nums1, nums2, i, j+1, k);
 
-        return dp[i][j][k]=Math.max(pick, Math.max(skip1, skip2));
-    }
+    //     return dp[i][j][k]=Math.max(pick, Math.max(skip1, skip2));
+    // }
 }
