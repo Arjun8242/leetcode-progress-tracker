06f26fb9cc69494d49e4d0786a23c1a5634e41d6
Arjun jaiswal <156437180+Arjun8242@users.noreply.github.com>
2026-05-29 00:19:27 +0530

    Time: 876 ms (8.76%), Space: 60 MB (16.6%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
-        List<List<Integer>> List=new ArrayList<>();
-       for(int i=0;i<nums.length;i++){
-        if(i>0 && nums[i]==nums[i-1]){
-            continue;
-        }
-        
-        int j=i+1;
-        int k=nums.length-1;
-        while(j<k){
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            //edgecase which i don't know
+
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
-                if(sum>0){
+                
+                if(sum==0){
+                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
                     k--;
                 }
-                else if(sum<0){
-                    j++;
+                else if(sum>0){
+                    k--;
                 }
                 else{
-                    List.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;
-                    k--;
-                    
-                    while(nums[j]==nums[j-1] && j<k){
-                       j++;
-                    }
-                    
                 }
             }
         }
-        return List;
-       }
-        
-        
+        List<List<Integer>> list = new ArrayList<>(set);
+        return list;
     }
+}
